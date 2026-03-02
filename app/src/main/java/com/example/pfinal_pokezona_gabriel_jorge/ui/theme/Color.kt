05dc8a9aaca75c2704074c52db88ad940ef584a3
Gabriel Almarcha <almarchagabriel@gmail.com>
2026-03-02 09:47:34 +0100

    Icono, type y theme

--- app/src/main/java/com/example/pfinal_pokezona_gabriel_jorge/ui/theme/Color.kt
+++ app/src/main/java/com/example/pfinal_pokezona_gabriel_jorge/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.pfinal_pokezona_gabriel_jorge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// MODO CLARO
+val LightFondo = Color(0xFFF5F5F5)
+val LightSuperficie = Color(0xFFFFFFFF)
+val LightPrimario = Color(0xFFFB1B1B)
+val LightAcento = Color(0xFF3B4CCA)
+val LightTitulos = Color(0xFF1D1D1D)
+val LightCuerpo = Color(0xFF4F4F4F)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// MODO OSCURO
+val DarkFondo = Color(0xFF1A1A1A)
+val DarkSuperficie = Color(0xFF2D2D2D)
+val DarkPrimario = Color(0xFFFF5353)
+val DarkAcento = Color(0xFF5B6DCB)
+val DarkTitulos = Color(0xFFF5F5F5)
+val DarkCuerpo = Color(0xFFBDBDBD)
